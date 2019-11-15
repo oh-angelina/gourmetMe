5fac5b6eb12ad9ca2fc9cd176be1a59d4bd1c29d
ohangelina <ohangelina@users.noreply.github.com>
2019-11-14 21:32:39 -0500

    attept to add reastaurant.html

--- src/main/java/com/angelinaopoku/gourmetMe/blogpost/BlogPostController.java
+++ src/main/java/com/angelinaopoku/gourmetMe/blogpost/BlogPostController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BlogPostController {
@@ -72,6 +74,11 @@ public class BlogPostController {
 	 public String deletePostWithId(@PathVariable Long id) {
 		 blogPostRepository.deleteById(id);
 		 return "redirect:/";
+	 }
+	 
+	 @RequestMapping(value = "/restaurants", method = RequestMethod.GET)
+	   public String redirect() {
+	      return "redirect:/blogpost/restaurants";
 	 }
 }
     
